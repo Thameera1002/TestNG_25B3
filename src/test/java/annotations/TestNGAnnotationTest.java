9e8b4099450b84d9e0a42cd99a65a5211c3f4c63
ChamodiWickramasinghe96 <chamodi1002@gmail.com>
2026-01-25 09:38:25 +0530

    testng.xml file and TestNGAnnotationTest.java updated

--- src/test/java/annotations/TestNGAnnotationTest.java
+++ src/test/java/annotations/TestNGAnnotationTest.java
@@ -25,8 +25,22 @@ public class TestNGAnnotationTest {
     }
 
     @Test
-    public void testMethod() {
-        System.out.println("I'm Test Method");
+    public void testMethod1() {
+        System.out.println("I'm Test Method1");
+    }
+
+    @Test
+    public void testMethod2() {
+        System.out.println("I'm Test Method2");}
+
+    @Test
+    public void testMethod3() {
+        System.out.println("I'm Test Method3");
+    }
+
+    @Test
+    public void testMethod4() {
+        System.out.println("I'm Test Method4");
     }
 
     @AfterMethod
