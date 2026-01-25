3c97e5f1da270d6eedc78eceadd7d730accc09ea
ChamodiWickramasinghe96 <chamodi1002@gmail.com>
2026-01-25 17:06:57 +0530

    ParallelExeTest.java added.

---
+++ src/test/java/parallel/ParallelExeTest.java
@@ -0,0 +1,57 @@
+package parallel;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class ParallelExeTest {
+
+    long startTime ;
+
+    @BeforeClass
+    public void beforeClass() {
+        startTime = System.currentTimeMillis();
+    }
+
+    @AfterClass
+    public void afterClass() {
+        long endTime = System.currentTimeMillis();
+        System.out.println("\n\nTotal Time taken: " + (endTime - startTime) + " milliseconds");
+    }
+
+    @Test
+    public void testMethod1() throws InterruptedException {
+        System.out.println("Test Method 1 - Thread ID: " + Thread.currentThread().getId());
+        for (int i = 0; i < 5; i++) {
+            Thread.sleep(1000);
+            System.out.println("Test Method 1 - Count: " + i);
+        }
+    }
+
+    @Test
+    public void testMethod2() throws InterruptedException {
+        System.out.println("Test Method 2 - Thread ID: " + Thread.currentThread().getId());
+        for (int i = 0; i < 5; i++) {
+            Thread.sleep(1500);
+            System.out.println("Test Method 2 - Count: " + i);
+        }
+    }
+
+    @Test
+    public void testMethod3() throws InterruptedException {
+        System.out.println("Test Method 3 - Thread ID: " + Thread.currentThread().getId());
+        for (int i = 0; i < 5; i++) {
+            Thread.sleep(2000);
+            System.out.println("Test Method 3 - Count: " + i);
+        }
+    }
+
+    @Test
+    public void testMethod4() throws InterruptedException {
+        System.out.println("Test Method 4 - Thread ID: " + Thread.currentThread().getId());
+        for (int i = 0; i < 5; i++) {
+            Thread.sleep(2500);
+            System.out.println("Test Method 4 - Count: " + i);
+        }
+    }
+}
