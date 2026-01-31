ad01bb72bee6a20e5b593c609f945879d4800198
Thameera Malinga <33679537+Thameera1002@users.noreply.github.com>
2026-01-31 10:16:54 +0530

    DependsOnTest.java added

---
+++ src/test/java/dependant/DependsOnTest.java
@@ -0,0 +1,33 @@
+package dependant;
+
+import org.testng.annotations.Test;
+
+public class DependsOnTest {
+
+    @Test
+    public void makeFoundation() {
+        System.out.println("Foundation is laid.");
+    }
+
+    @Test(dependsOnMethods = {"makeFoundation"})
+    public void makeWalls() {
+        System.out.println("Walls are built.");
+    }
+
+    @Test(dependsOnMethods = {"makeFoundation","makeWalls"})
+    public void makeRoof() {
+        System.out.println("Roof is constructed.");
+    }
+
+    @Test(dependsOnMethods = {"makeFoundation","makeWalls","makeRoof"})
+    public void supplyElectricity() {
+        System.out.println("Electricity is supplied.");
+    }
+
+    @Test(dependsOnMethods = {"makeFoundation","makeWalls","makeRoof"})
+    public void supplyWater() {
+        System.out.println("Water is supplied.");
+    }
+
+
+}
